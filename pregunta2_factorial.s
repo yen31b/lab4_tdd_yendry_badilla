c4c8c1659db48e541f197b8a069ba82593994427
Yendry Badilla <38542884+yen31b@users.noreply.github.com>
2025-05-21 01:09:52 -0600

    pregunta 2, mejora de comentarios

--- pregunta2_factorial.s
+++ pregunta2_factorial.s
@@ -3,16 +3,16 @@
 _start:
 
 mov r0, #1 // resultado
-mov r1, #5 // valor de n
+mov r1, #5 // valor de n, n=5
 
 loop:
 
-cmp r1, #1 //compara r1 con 1
-ble _end   // termina 
-mul r0, r1 //multiplicacion
-sub r1, r1, #1 // n - 1
+cmp r1, #1 //compara r1 (n) con 1
+ble _end   // termina si r1(n) es = 1
+mul r0, r1 //multiplicacion resultado = resultado*n
+sub r1, r1, #1 // n = n - 1
 bal loop
 
 _end:
-mov r7, #1
-swi #0	
+mov r7, #1 //return de int
+swi #0	// interrupcion para terminar
